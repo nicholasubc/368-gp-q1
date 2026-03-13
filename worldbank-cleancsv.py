137619d6ade2464e49e04fb4f08603771987c60f
Nicholas Mui <>
2026-03-12 22:09:40 -0700

    clean world bank

---
+++ worldbank-cleancsv.py
@@ -0,0 +1,21 @@
+import csv
+
+data = "data/11e258b4-4a6a-456f-bca5-52cea556de9a_Data.csv"
+csvout = "out/worldbank.csv"
+with open(data, mode="r", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+
+    rows = []
+    for row in reader:
+        name = row["Country Name"]
+        code = row["Series Code"]
+        val = row["2024 [YR2024]"]
+        if val == "..":  # .. means null
+            val = ""
+        rows.append([name, code, val])
+
+rows = rows[:-5]
+with open(csvout, mode="w", encoding="utf-8", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Country Name", "Series Code", "LatestValue"])
+    writer.writerows(rows)
